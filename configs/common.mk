5fd1cea5bbfd5dec7f25ad74310b877620fb74e2
Yuyuko1024 <dingwenxuan4@gmail.com>
2024-08-01 20:51:15 +0800

    configs: 添加基础构建规则makefile

---
+++ configs/common.mk
@@ -0,0 +1,31 @@
+#
+# Copyright (C) 2022 The exTHmUI Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# exTHmUI version
+-include vendor/exthmui/config/version.mk
+
+# Sounds
+include vendor/exthmui/config/sounds.mk
+
+# Include {Lato,Rubik} fonts
+$(call inherit-product-if-exists, external/google-fonts/lato/fonts.mk)
+$(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
+
+# Media
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    media.recorder.show_manufacturer_and_model=true
+
+# Tethering - allow without requiring a provisioning app
+# (for devices that check this)
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    net.tethering.noprovisioning=true
+
+# Override undesired Google defaults
+PRODUCT_PRODUCT_PROPERTIES += \
+    keyguard.no_require_sim=true \
+    ro.setupwizard.enterprise_mode=1 \
+    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html
---
+++ configs/version.mk
@@ -0,0 +1,24 @@
+#
+# Copyright (C) 2022 The exTHmUI Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Version
+EXTHM_NUM_VERSION := 11.0
+EXTHM_BRANCH := Renko
+EXTHM_BUILD_TYPE := DEBUG
+EXTHM_DATE := $(shell date -u +%Y%m%d)
+EXTHM_DEVICE := $(shell echo "$(TARGET_PRODUCT)" | cut -d '_' -f 2,3)
+
+ifeq ($(IS_RELEASE), true)
+    EXTHM_BUILD_TYPE := RELEASE
+endif
+
+EXTHM_VERSION := exTHmUI-$(EXTHM_NUM_VERSION)-$(EXTHM_BRANCH)-$(EXTHM_DEVICE)-$(EXTHM_BUILD_TYPE)-$(EXTHM_DATE)
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.exthm.version=$(EXTHM_VERSION) \
+    ro.exthm.branch=$(EXTHM_BRANCH) \
+    ro.exthm.build.version=$(EXTHM_NUM_VERSION) \
+    ro.exthm.build.type=$(EXTHM_BUILD_TYPE)
